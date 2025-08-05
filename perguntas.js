5dfad616bc263afd3754d77144cf68793a8c1f23
willianpacheco15 <willian.santos.pacheco@escola.pr.gov.br>
2025-08-05 10:49:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    '',
+    'Quanto é 5+8−2?',
+    '11'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'matematica',
+    ' Quantos lados tem um triângulo?',
+    '3'
+)
+
+criaCartao(
+    'matematica',
+    'Qual é o número que vem depois do 9?',
+    '10,'
+)
